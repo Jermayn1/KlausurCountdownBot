d7ad10ac559cc0466617e664d47578a3ed3e846f
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-11-03 15:35:28 +0100

    Update timerChannel.js

--- src/Structures/Systems/timerChannel.js
+++ src/Structures/Systems/timerChannel.js
@@ -4,7 +4,7 @@ const channelsSoop = ["1428797577595195432", "1428797805052297409"];
 
 // Klausur-Daten
 const examDates = {
-    GDI: new Date("2026-02-09T00:00:00Z"),  // GDI am 09.02.2026
+    GDI: new Date("2026-02-06T00:08:00Z"),  // GDI am 09.02.2026
     SOOP: new Date("2026-01-30T00:00:00Z"), // SOOP am 30.01.2026
 };
 
